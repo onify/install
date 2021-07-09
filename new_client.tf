9beae36e9e101f914dca5cdf3716b1357cca8a78
Robert Lundsten <robert@onify.co>
2021-07-09 20:15:45 +0200

    fix name and path

--- new_client.tf
+++ new_client.tf
@@ -1,8 +1,8 @@
-module "onify-client-ioforce" {
-  source           = "git::git@github.com:onify/install//terraform/modules/onify-customer?ref=terraform"
-  #source               = "./terraform/modules/onify-customer"
+module "onify-client-oni" {
+  #source           = "git::git@github.com:onify/install//terraform/modules/onify-customer?ref=terraform"
+  source               = "./terraform/modules/onify-customer"
   gcr_registry_keyfile = "~/repos/onify/keyfile.json"
-  onify_client_code    = "ioforce"
+  onify_client_code    = "oni"
   onify_instance       = "demo1"
   //onify-agent_external     = true // creates ingress with external access
   onify-agent_version         = "latest"
