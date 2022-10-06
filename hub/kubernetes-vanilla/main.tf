65bbf6e692475422a4b1dcbca46fd43a1346188d
Robert Lundsten <robert@onify.co>
2022-10-06 16:04:47 +0200

    update terraform file

--- hub/kubernetes-vanilla/main.tf
+++ hub/kubernetes-vanilla/main.tf
@@ -1,25 +1,29 @@
+/*
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+  config_context = "gke_onify-forge_europe-north1-a_onify-demo-gke"
+}
+*/
+
 variable "client_code" {
+    default = "xxx" // Contact your local partner or support@onify.co to get this information
 }
 variable "client_instance" {
+    default = "xxx" // Contact your local partner or support@onify.co to get this information
 }
 
 module "onify" {
-  source                         = "git::git@github.com:onify/install//terraform/modules/kubernetes-vanilla"
-  gcr_registry_keyfile            = "~/repos/onify/keyfile.json"
+  source                          = "git::git@github.com:onify/install//terraform/modules/kubernetes-vanilla"
+  gcr_registry_keyfile            = "./keyfile.json" // Contact your local partner or support@onify.co to get this information
   onify_api_envs                  = {
     ONIFY_client_code             = var.client_code
     ONIFY_client_instance         = var.client_instance
-    ONIFY_initialLicense          = "xxx"
+    ONIFY_initialLicense          = "xxx" // Contact your local partner or support@onify.co to get this information
     ONIFY_adminUser_password      = "xxx"
     ONIFY_apiTokens_app_secret    = "xxx"
     ONIFY_client_secret           = "xxx"
   }
   onify_app_envs                    = {
-    ONIFY_api_externalUrl         = "/api/v2"
-    ONIFY_disableAdminEndpoints   = true
     ONIFY_api_admintoken          = "xxx"
-  }
-  onify_worker_envs               = {
-    ONIFY_worker_cleanupInterval  = 30
   }
 }
